e7482db7f877096e415f6fb5f2cbb673f4536533
ViniTVS <vinisantos185@gmail.com>
2023-08-28 17:02:58 -0300

    Inicio desenho igual do prof :P

--- pages/algoritmos/produtor.vue
+++ pages/algoritmos/produtor.vue
@@ -4,7 +4,7 @@
     <div class="grow" ref="d3Container" id="d3Container"></div>
     <div id="comandos" class=" m-auto" style="height: 60px;">
       <div class="join">
-        <button class="btn join-item" v-on:click="addProduto">Adiciona produto</button>
+        <button class="btn join-item" v-on:click="criaProdutor">Adiciona produto</button>
         <button class="btn join-item" v-on:click="removeProduto">Remove produto</button>
         <button class="btn join-item">Button</button>
       </div>
@@ -36,130 +36,96 @@ useHead({
 });
 
 
+const TAM_QUADRADO = 20;
+const TAM_CIRCULO = 30;
+
 let canvas = ref(null);
 let containerW = ref(null);
 let containerH = ref(null);
-let tam_quadrado = 20;
 let rotulos = ref({
   obj: [],
   texto: ["Produtor", "Buffer", "Consumidor"]
 });
-let produtos = ref([]);
+let produtores = ref([]);
 let buffer = ref([]);
 let consumidos = ref([]);
 
 
+watch(containerW, () => {
+  // desenhaProdutores();
+  desenhaProdutores();
+});
+
 onMounted(() => {
-  createWelcomePage();
+  console.log(produtores.value);
+  // createWelcomePage();
   // criaCirculo();
   handleResize();
   window.addEventListener('resize', handleResize);
-  for (let i = 0; i < rotulos.value.texto.length; i++) {
-    var t = canvas.value.append("text")
-      .text(rotulos.value.texto[i])
-      .attr("x", containerW.value * (i + 1) / 4)
-      .attr("text-anchor", "middle")
-      .attr("y", containerH.value * 0.70);
-    rotulos.value.obj.push(t);
-  }
 
-})
-
-function createWelcomePage() {
   canvas.value = d3.select("#d3Container")
     .append('svg')
     .attr('width', '100%')
     .attr('height', '100%');
+    
+  criaProdutor();
+  criaProdutor();
+
+})
+
+function desenhaProdutores() {
+  let num_prod = produtores.value.length;
+
+  for (let i = 0; i < num_prod; i++) {
+    produtores.value[i].objeto
+      .attr("cx", containerW.value / 4)
+      .attr("cy", containerH.value * (i + 1) / (num_prod + 1));
+    produtores.value[i].texto
+    .text("p" + (i + 1))
+    .attr("dx", containerW.value / 4)
+    .attr("dy", containerH.value * (i + 1) / (num_prod + 1) + 4);
+  }
 }
+
+function criaProdutor() {
+
+  var objeto = canvas.value.append("circle")
+    .attr("r", TAM_CIRCULO)
+    .style("stroke-width", 5)
+    .style("stroke", "black")
+    .style("fill", "cyan");
+  
+  var texto = canvas.value.append("text")
+    .attr("text-anchor", "middle")
+    .attr("dx", 100)
+    .attr("dy", 100)
+    .text("teste");
+
+
+  produtores.value.push({
+    objeto: objeto,
+    texto: texto
+  });
+  desenhaProdutores();
+}
+
+function removeProdutor() {
+
+  var t = canvas.value.append("text")
+    .text(rotulos.value.texto[i])
+    .attr("x", containerW.value * (i + 1) / 4)
+    .attr("text-anchor", "middle")
+    .attr("y", containerH.value * 0.70);
+  rotulos.value.obj.push(t);
+}
+
 
 function handleResize() {
   containerW.value = document.getElementById("d3Container").offsetWidth;
   containerH.value = document.getElementById("d3Container").offsetHeight;
-  desenhaProdutos();
+  // desenhaProdutos();
 }
-function addProduto() {
-  if (produtos.value.length >= 5)
-    return;
-
-  var produto = canvas.value.append("rect")
-    .attr("width", tam_quadrado)
-    .attr("height", tam_quadrado)
-    .attr('x', containerW.value / 4)
-    .attr('y', containerH.value * 0.60 - 30 * produtos.value.length)
-    .attr('fill', 'transparent');
-  // depois de usar transition ele retorna o elemento da transition, não
-  // o objeto original, então é necessário separá-los
-  produto.transition().duration(250).attr('fill', 'green');
-
-  produtos.value.push(produto);
-
-}
-function removeProduto() {
-  if (produtos.value.length == 0)
-    return;
-
-  produtos.value[0]
-    .transition()
-    .duration(250)
-    .attr('fill', 'transparent')
-    .remove();
-  produtos.value.shift();
-
-  for (let i in produtos.value) {
-    produtos.value[i]
-      .transition()
-      .delay(250)
-      .duration(750)
-      .attr('x', containerW.value / 4)
-      .attr('y', containerH.value * 0.60 - 30 * i);
-  }
-}
-// function desenaRotulos() {
-
-// }
-function desenhaProdutos() {
-  for (let i in produtos.value) {
-    produtos.value[i]
-      .attr('x', containerW.value / 4)
-      .attr('y', containerH.value * 0.60 - 30 * i);
-  }
-}
-function criaCirculo() {
-  var circle = canvas.value.append('circle')
-    .attr('cx', 250)
-    .attr('cy', 250)
-    .attr('r', 50)
-    .attr('fill', 'red');
-
-  circle.transition()
-    .duration(1500)
-    .delay(2000)
-    .attr('cx', 100)
-    .attr('cy', 100)
-    .transition()
-    .attr('fill', 'blue');
-
-  circle.on('mouseover', function () {
-    d3.select(this)
-      .transition()
-      .attr('r', 100)
-      .attr('fill', 'green');
-  });
 
 
-  circle.on('mouseout', function () {
-    d3.select(this)
-      .transition()
-      .attr('r', 50)
-      .attr('fill', 'red');
-  });
-
-  circle.on('click', function () {
-    d3.select(this)
-      .transition()
-      .attr('r', 200)
-      .attr('fill', 'yellow');
-  });
-}
 
 </script>
