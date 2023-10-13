5a8b799f5a01e1e55589bfcf3b2a90444d160083
Glaide de Lara Oliveira <glaide@craftpro.com.br>
2023-10-12 22:48:50 -0300

    MORRAM TODOS

--- pages/algoritmos/escalonamento.vue
+++ pages/algoritmos/escalonamento.vue
@@ -2,7 +2,27 @@
 <template>
   <div class="flex flex-col h-full">
     <!-- <div class="grow">a</div> -->
-    <div class="flex flex-row items-start">
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th v-for="header in headers" :key="header">{{ header }}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="row in data" :key="row.id">
+            <td v-for="(cell, index) in row.cells" :key="index">
+              <span v-if="cell.id === 1" style="padding: 8px">{{ cell.value }}</span>
+              <span v-else-if="cell.id === 7" style="padding: 8px">{{ cell.value }}</span>
+              <span v-else-if="cell.id === 13" style="padding: 8px">{{ cell.value }}</span>
+              <input v-else type="number" v-model="cell.value" @change="updateTarefas(row.id, cell.value)" min="0"
+                max="5">
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class=" flex flex-row items-start">
       <div class="flex flex-col h-full justify-evenly" style="height: 274px; padding-left: 10px;">
         <p>t1</p>
         <p>t2</p>
@@ -31,13 +51,48 @@
 
 <script setup>
 
+
 import * as d3 from "d3";
-import { ref, computed, onMounted } from 'vue';
+
+
+import { ref, computed, onMounted, } from 'vue';
 
 useHead({
   title: 'Produtor-consumidor - VisualSO'
 });
+const headers = ["Tarefa", "t1", "t2", "t3", "t4", "t5"];
+const data = [
+  {
+    id: 1, cells: [
+      { id: 1, value: "ingresso" },
+      { id: 2, value: "0" },
+      { id: 3, value: "1" },
+      { id: 4, value: "2" },
+      { id: 5, value: "2" },
+      { id: 6, value: "3" }]
+  },
+  {
+    id: 2, cells: [
+      { id: 7, value: "duração" },
+      { id: 8, value: "5" },
+      { id: 9, value: "2" },
+      { id: 10, value: "4" },
+      { id: 11, value: "1" },
+      { id: 12, value: "2" }
+    ]
+  },
+  {
+    id: 3, cells: [
+      { id: 13, value: "prioridade" },
+      { id: 14, value: "2" },
+      { id: 15, value: "3" },
+      { id: 16, value: "1" },
+      { id: 17, value: "4" },
+      { id: 18, value: "5" }
 
+    ]
+  }
+];
 // cores para usar ao criar prdutores. Também limita o num. de produtores (1 pra cada cor)
 const cores = [
   // "hsl(var(--p))",
@@ -47,7 +102,7 @@ const cores = [
   "hsl(var(--wa))",
   "hsl(var(--er))",
 ];
-const cores_estado= [
+const cores_estado = [
   "hsl(var(--b1))",
   "hsl(var(--b3))",
 ]
@@ -106,6 +161,18 @@ let tarefas = [
 
 
 
+function updateTarefas(id, value) {
+
+  if (id >= 2 && id <= 6) {
+    tarefas[id - 2].ingresso = value;
+  } if (id >= 8 && id <= 12) {
+    tarefas[id - 8].duracao = value;
+  } if (id >= 14 && id <= 18) {
+    tarefas[id - 14].prioridade = value;
+  }
+
+}
+
 watch(containerW, () => {
 });
 
@@ -154,9 +221,9 @@ onMounted(() => {
 function firstComefirstServed(tarefas, indice_atual = -1, tempo = 0) {
   var novo_indice = indice_atual;
   dd(tarefas[novo_indice]);
-  for(let i = 0; i < tarefas.length; i++){
-    if(tarefas[i].ingresso <= tempo){
-      if(novo_indice == -1){
+  for (let i = 0; i < tarefas.length; i++) {
+    if (tarefas[i].ingresso <= tempo) {
+      if (novo_indice == -1) {
         novo_indice = i;
       }
     }
@@ -191,6 +258,8 @@ function dd() {
   for (var i = 0; i < arguments.length; ++i)
     console.log(arguments[i]);
 }
+
+
 
 function getColorByState(estado, lin) {
   if (estado == 0) return cores_estado[0];
